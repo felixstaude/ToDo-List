36aebbbb9cd731f65bdfcd183f7bd81e8deafb75
felixstaude <felixstaude0112@gmail.com>
2022-08-19 23:48:11 +0200

    initial commit

---
+++ src/de/felixstaude/gui/Gui.java
@@ -0,0 +1,48 @@
+package de.felixstaude.gui;
+
+import com.melloware.jintellitype.HotkeyListener;
+import com.melloware.jintellitype.JIntellitype;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame {
+
+    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private int heightFrame = 500;
+    private int widthFrame = 200;
+    private int heightScreen = screenSize.height;
+    private int widthScreen = screenSize.width;
+    private int posXFrame = widthScreen - widthFrame - 5;
+    private int posYFrame = (heightScreen / 2) - (heightFrame / 2);
+
+    public Gui(){
+        setResizable(false);
+        setTitle("ToDo");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(posXFrame, posYFrame, widthFrame, heightFrame);
+        // no header
+        setUndecorated(true);
+
+
+        setVisible(true);
+        // set hotkeys
+        JIntellitype.getInstance().addHotKeyListener(new HotkeyListener() {
+            @Override
+            public void onHotKey(int i) {
+                if(i == 1){
+                    if(isVisible()){
+                        // open/ close window
+                        setVisible(false);
+                    } else {
+                        setVisible(true);
+                    }
+                } else if (i == 2){
+                    // kill process
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
+}
---
+++ src/de/felixstaude/main/Main.java
@@ -0,0 +1,23 @@
+package de.felixstaude.main;
+
+import com.melloware.jintellitype.HotkeyListener;
+import com.melloware.jintellitype.JIntellitype;
+import de.felixstaude.gui.Gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
+    public static void main(String[] args){
+        new Gui();
+        // register hotkeys
+        JIntellitype.getInstance().registerHotKey(1, JIntellitype.MOD_CONTROL, (int)'L');
+        JIntellitype.getInstance().registerHotKey(2, JIntellitype.MOD_CONTROL, (int)'K');
+
+
+    }
+
+
+}
+
